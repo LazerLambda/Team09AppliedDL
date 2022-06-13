d0f8ddb38d5749091323a88a86411836f6c0a63a
Philipp Koch <PhillKoch@protonmail.com>
2022-06-13 02:55:18 +0200

    Update tox.ini, refactor ImbalancedLoss.py and test_ImbalancedLoss.py

--- src/ImbalancedLoss.py
+++ src/ImbalancedLoss.py
@@ -1,11 +1,12 @@
-from typing import Any, Tuple, Union
+"""Class for Loss Function for Imbalanced Data."""
+
+from typing import Union
 
 import torch
-from numpy import isin
 
 
 class ImbalancedLoss:
-    """Loss Function for"""
+    """Loss Function Class for Imbalanced Data."""
 
     def __init__(
         self, device: Union[str, torch.device], p: float, p_: float = 0.5
@@ -29,86 +30,86 @@ class ImbalancedLoss:
     def sum_exp(self, x: torch.Tensor, sgn: int) -> torch.Tensor:
         """Compute Sum over Sigmoid Loss Tensor.
 
-        :param device: Device to compute on.
+        :param x: Tensor with entries to sum up.
+        :param sgn: Sign for sigmoid loss fucntion.
 
         :return: Sum of tensor.
         """
-        return torch.sum(
-            self.sigmoid_loss(
-                output=x,
-                label=(
-                    sgn
-                    * torch.ones((x.shape[0] if len(x.shape) > 0 else 0)).to(
-                        self.device
-                    )
-                ),
-            )
-        )  # TODO: Remove?
+        x_shape: int = x.shape[0] if len(x.shape) > 0 else 0
+        # TODO: Remove device here?
+        label: torch.Tensor = sgn * torch.ones(x_shape).to(self.device)
+        return torch.sum(self.sigmoid_loss(x, label))
 
     @staticmethod
-    def sigmoid_loss(output: torch.Tensor, label: torch.Tensor) -> torch.Tensor:
+    def sigmoid_loss(
+            output: torch.Tensor,
+            label: torch.Tensor) -> torch.Tensor:
         """Compute Sigmoid Loss Function.
 
         Compute:
         :param output: predicted value.
-        :param device: Device to compute on.
+        :param label: Ground truth labels.
 
-        :return: loss
+        :return: loss for input.
         """
         product: torch.Tensor = torch.mul(output, label)
         denum: torch.Tensor = torch.add(1, torch.exp(product))
         return torch.div(1, denum)
 
     def imbalanced_nnpu(
-        self, pred_p: torch.Tensor, pred_u: torch.Tensor
-    ) -> torch.Tensor:
-        """Compute ImbalancednnPU-Loss
+            self,
+            pred_p: torch.Tensor,
+            pred_u: torch.Tensor) -> torch.Tensor:
+        """Compute ImbalancednnPU-Loss.
 
-        Implementation according to https://www.ijcai.org/proceedings/2021/0412.pdf
+        Implementation according to
+        https://www.ijcai.org/proceedings/2021/0412.pdf.
 
         :param pred_p: Positive labeled data.
         :param pred_u: Unlabeled data.
-        :param p: Proportion of labeled to unlabeled data.
-        :param p_: Proportion of upsampled data.
-        :param device: Device to compute on.
 
         :returns: Loss for batch.
         """
         np: int = pred_p.shape[0] if len(pred_p.shape) > 0 else 0
-        cp: float = (1 - self.p_) * self.p / (np * (1 - self.p)) if np != 0 else 0
+        cp: float = (1 - self.p_) * self.p / (np * (1 - self.p))\
+            if np != 0 else 0
 
         nu: int = pred_u.shape[0] if len(pred_u.shape) > 0 else 0
-        cu: float = (1 - self.p_) / (nu * (1 - self.p)) if nu != 0 else 0
+        cu: float = (1 - self.p_) / (nu * (1 - self.p))\
+            if nu != 0 else 0
 
         sum1: torch.Tensor = self.sum_exp(pred_u, -1)
         sum2: torch.Tensor = self.sum_exp(pred_p, -1)
 
         return cu * sum1 - cp * sum2
 
-    def nn_balancePN(self, pred_p: torch.Tensor, pred_u: torch.Tensor) -> torch.Tensor:
+    def nn_balance_pn(
+            self,
+            pred_p: torch.Tensor,
+            pred_u: torch.Tensor) -> torch.Tensor:
         """Compute nnBalancePN.
 
         Implementation according to https://arxiv.org/abs/1703.00593.
 
         :param pred_p: Positive labeled data.
         :param pred_u: Unlabeled data.
-        :param p: Proportion of labeled to unlabeled data.
-        :param p_: Proportion of upsampled data.
-        :param device: Device to compute on.
 
         :returns: Loss for batch.
         """
-        clipped_imbnnPU: torch.Tensor = torch.max(
+        clipped_imb_nnpn: torch.Tensor = torch.max(
             torch.Tensor([0]).to(self.device),
             self.imbalanced_nnpu(pred_p=pred_p, pred_u=pred_u),
         )
         np: int = pred_p.shape[0]
         if np == 0:
-            return clipped_imbnnPU
+            return clipped_imb_nnpn
         else:
-            return self.p_ / np * self.sum_exp(pred_p, 1) + clipped_imbnnPU
+            return self.p_ / np * self.sum_exp(pred_p, 1) + clipped_imb_nnpn
 
-    def __call__(self, predictions: torch.Tensor, labels: torch.Tensor) -> torch.Tensor:
+    def __call__(
+            self,
+            predictions: torch.Tensor,
+            labels: torch.Tensor) -> torch.Tensor:
         """Execute on Call.
 
         :param predictions: Tensor of predictions from model.
@@ -121,6 +122,6 @@ class ImbalancedLoss:
 
         if self.imbalanced_nnpu(pred_p, pred_u) >= 0:
             # TODO: Sign correct?
-            return self.nn_balancePN(pred_p, pred_u)
+            return self.nn_balance_pn(pred_p, pred_u)
         else:
             return -self.imbalanced_nnpu(pred_p, pred_u)
--- tests/test_ImbalancedLoss.py
+++ tests/test_ImbalancedLoss.py
@@ -3,35 +3,34 @@
 import pytest
 import torch
 
-from src import ImbalancedLoss as il
+from src import ImbalancedLoss as ImbL
 
 n = 2000
 
 
 def test_1():
     """Test for Correct Parameters."""
-
-    _ = il.ImbalancedLoss("cpu", 0.2)
-
-    with pytest.raises(Exception):
-        il.ImbalancedLoss("cpu", -1, 2)
+    _ = ImbL.ImbalancedLoss("cpu", 0.2)
 
     with pytest.raises(Exception):
-        il.ImbalancedLoss("cpu", -1, 0.5)
+        ImbL.ImbalancedLoss("cpu", -1, 2)
 
     with pytest.raises(Exception):
-        il.ImbalancedLoss("cpu", -1, 0.5)
+        ImbL.ImbalancedLoss("cpu", -1, 0.5)
 
     with pytest.raises(Exception):
-        il.ImbalancedLoss(torch.device("cpu"), 0.5, -0.5)
+        ImbL.ImbalancedLoss("cpu", -1, 0.5)
 
     with pytest.raises(Exception):
-        il.ImbalancedLoss(-2, 0.5, -0.5)
+        ImbL.ImbalancedLoss(torch.device("cpu"), 0.5, -0.5)
+
+    with pytest.raises(Exception):
+        ImbL.ImbalancedLoss(-2, 0.5, -0.5)
 
 
 def test_2():
     """Check `sigmoid_loss` for correct properties."""
-    loss = il.ImbalancedLoss("cpu", 0.5)
+    loss = ImbL.ImbalancedLoss("cpu", 0.5)
 
     with pytest.raises(Exception):
         loss.sigmoid_loss(torch.rand(20), torch.rand(2))
@@ -50,7 +49,7 @@ def test_2():
 
 def test_3():
     """Check `sum_exp` for correct shape of output and type."""
-    loss = il.ImbalancedLoss("cpu", 0.5)
+    loss = ImbL.ImbalancedLoss("cpu", 0.5)
     res = loss.sum_exp(torch.rand(n), -1)
     assert len(res.shape) == 0
     assert isinstance(res, torch.Tensor)
@@ -58,14 +57,14 @@ def test_3():
 
 def test_4():
     """Test  `imbalanced_nnpu` for correct properties."""
-    loss = il.ImbalancedLoss("cpu", 0.5)
+    loss = ImbL.ImbalancedLoss("cpu", 0.5)
     assert loss.imbalanced_nnpu(torch.tensor([]), torch.tensor([])) == 0
     assert loss.imbalanced_nnpu(torch.rand(n), torch.rand(n)) >= 0
 
 
 def test_5():
-    """Test  `nn_balancePN` for correct properties."""
-    loss = il.ImbalancedLoss("cpu", 0.5)
+    """Test  `nn_balance_pn` for correct properties."""
+    loss = ImbL.ImbalancedLoss("cpu", 0.5)
     p_set = torch.tensor(n)
     u_set = torch.tensor(n * 2)
     assert loss.imbalanced_nnpu(torch.tensor([]), torch.tensor(n)) >= 0
@@ -76,5 +75,6 @@ def test_5():
 
 def test_6():
     """Test  `__call__` for correct behavior."""
-    loss = il.ImbalancedLoss("cpu", 0.5)
-    assert loss(torch.rand(n), torch.sign(torch.empty(n).random_(2) - 0.5)) >= 0
+    loss = ImbL.ImbalancedLoss("cpu", 0.5)
+    assert\
+        loss(torch.rand(n), torch.sign(torch.empty(n).random_(2) - 0.5)) >= 0
