9789399a59df82d9520cbda5f6f9a8ccac4cdf70
Philipp Koch <PhillKoch@protonmail.com>
2022-07-07 11:03:32 +0200

    Add transformer

--- src/models/Teachers.py
+++ src/models/Teachers.py
@@ -6,7 +6,7 @@ class Teachers:
     """Container Class for Teacher-Models."""
 
     @staticmethod
-    def get_debug_teacher():
+    def get_debug_teacher() -> nn.Module:
         """Return Simple Teacher Model for Debugging.
 
         :return: Simple Model.
@@ -20,9 +20,28 @@ class Teachers:
         )
 
     @staticmethod
-    def get_transformer():
+    def get_transformer() -> nn.Module:
         """Return Transformer.
 
         :return: Transformer.
         """
-        return None  # TODO
+        # https://pytorch.org/docs/stable/generated/torch.nn.TransformerEncoder.html
+        return nn.Sequential(
+            # https://pytorch.org/docs/stable/generated/torch.nn.TransformerEncoderLayer.html
+            nn.Flatten(1, -1),
+            nn.TransformerEncoder(
+                encoder_layer=nn.TransformerEncoderLayer(
+                    d_model=125,
+                    nhead=5,
+                    dim_feedforward=2048,
+                    activation=nn.functional.relu,
+                    layer_norm_eps=1e-5,
+                    batch_first=False,
+                    norm_first=False
+                ),
+                num_layers=1,
+                norm=None
+            ),
+            nn.Linear(125, 1),
+            nn.Sigmoid()
+        )
