88aa85986908c3e1e99e667a4b127145ef92411a
Philipp Koch <PhillKoch@protonmail.com>
2022-07-22 12:01:54 +0200

    Add hyperband confi generator

---
+++ scripts/createHyperbandConfigs.sh
@@ -0,0 +1,6 @@
+hyband generate 3 3 \
+    --guaranteed-budget 3 \
+    --cost-one-epoch-full-dataset 0.0028 \
+    --output-dir default_config \
+    --random-seed 123456 \
+    --template-dir config/templates/
