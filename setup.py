66bbb315605f60c2ebf907e73c8d3e1a9c0a677b
Philipp Koch <PhillKoch@protonmail.com>
2022-08-22 15:06:16 +0200

    Add description string

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='src',
     packages=find_packages(),
     version='0.1.0',
-    description='tba',
+    description='Online Model Distillation.\nProject for the applied deep learning class (Summer Semester 2022).',
     author='Gjeneta Ramadani, Philipp Koch, Simon Schosser',
     license='Apache License, Version 2.0',
 )
